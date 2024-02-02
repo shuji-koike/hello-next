f23e9270aad5dd2ffdbfb955adbe51a312006e2e
Shuji Koike <shuji.koike@me.com>
2024-02-02 15:00:01 +0900

    fixup! [ci skip] WIP WIP WIP WIP WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { AuthHeader } from "./auth";
 import { supabase } from "@/lib/supabase";
 import { sql } from "@vercel/postgres";
@@ -13,13 +14,30 @@ export default async function Home() {
   // await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
   const { rows } = await sql`SELECT * from Pets`;
   console.log(rows);
-  await sql`INSERT INTO Pets (Name, Owner) VALUES ('Fido', 'Joe');`;
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <header className="flex items-start gap-3">
         <AuthHeader />
       </header>
+      <article className="p-4">
+        <form
+          action={async function Post() {
+            "use server";
+          }}
+        >
+          <input type="text" name="name" placeholder="title" />
+          <input type="text" name="owner" placeholder="memo" />
+          <Button>save</Button>
+        </form>
+        {rows.map((row) => (
+          <>
+            <div>
+              <p>Title: {row.name}</p>
+              <p>Memo: {row.owner}</p>
+            </div>
+          </>
+        ))}
+      </article>
     </main>
   );
 }
